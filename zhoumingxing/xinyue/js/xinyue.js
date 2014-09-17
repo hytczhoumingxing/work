17e38c257a7c8679134e42d49c9894e60c030f71
hytczhoumingxing <1027883520@qq.com>
2014-09-17 16:24:21 +0800

    2014-9-17
    开始心悦酒店的网页

---
+++ zhoumingxing/xinyue/js/xinyue.js
@@ -0,0 +1,15 @@
+$(function(){
+	$(".menu ul li").hover(function(){
+
+	},function(){});
+});
+
+/*function show_date() {
+	var dt_day = dt_month = dt_weekday = '';
+	var dt_week = [L['Sunday'], L['Monday'], L['Tuesday'], L['Wednesday'], L['Thursday'], L['Friday'], L['Saturday']];
+	dt_today = new Date();
+	dt_weekday = dt_today.getDay();
+	dt_month = dt_today.getMonth()+1;
+	dt_day = dt_today.getDate();
+	document.write(lang(L['show_date'], [dt_month, dt_day, dt_week[dt_weekday]]));
+}*/
